528d96405d0084a6a18110507964e8d0713b4237
Diogo Azevedo <diogoazevedos@gmail.com>
2016-12-22 10:22:01 -0200

    feat: Add graphql server registers

--- src/server.js
+++ src/server.js
@@ -1,10 +1,56 @@
+const { merge } = require('lodash');
 const { Server } = require('hapi');
+const { makeExecutableSchema } = require('graphql-tools');
+const { graphqlHapi, graphiqlHapi } = require('graphql-server-hapi');
 
 const server = new Server();
+
+const rootSchema = `
+  type Query {
+    quote: String
+  }
+
+  schema {
+    query: Query
+  }
+`;
+
+const rootResolvers = {
+  Query: {
+    quote() {
+      return 'Simplicity is the ultimate sophistication. - Leonardo da Vinci';
+    },
+  },
+};
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: [rootSchema],
+  resolvers: merge(rootResolvers),
+});
 
 server.connection({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || '3000',
+});
+
+server.register({
+  register: graphqlHapi,
+  options: {
+    path: '/graphql',
+    graphqlOptions: {
+      schema: executableSchema,
+    },
+  },
+});
+
+server.register({
+  register: graphiqlHapi,
+  options: {
+    path: '/graphiql',
+    graphiqlOptions: {
+      endpointURL: '/graphql',
+    },
+  },
 });
 
 module.exports = server;
